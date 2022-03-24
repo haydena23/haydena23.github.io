ca1ea202130421be4daf4cccfba9e18c38297ba3
morganco23 <morganco23@up.edu>
2022-03-23 19:16:51 -0700

    added request module and code to get token

---
+++ test.js
@@ -0,0 +1,40 @@
+const request = require('request');
+
+var client_id = '8d2bba2a92db40bd8b107fbe226957b9';
+var client_secret = '53dcaf963d5348798fc5e3fce02117f6';
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+var token = 'BQDTobEeNbUIAcLaxWCYv-dCxpZYsEzVu4JsYGoncAqHq7uOq4BAWI5NkevK-z76TRV08Fo2kPk5MB77zxE';
+// request.post(authOptions, function(error, response, body) {
+//   if (!error && response.statusCode === 200) {
+//     token = body.access_token;
+//   }
+//   else{
+//       token='BQDTobEeNbUIAcLaxWCYv-dCxpZYsEzVu4JsYGoncAqHq7uOq4BAWI5NkevK-z76TRV08Fo2kPk5MB77zxE'
+//   }
+// });
+var getOptions = {
+    url: 'https://api.spotify.com/v1/tracks/11dFghVXANMlKmJXsNCbNl',
+    headers: {
+        'Authorization': 'Bearer ' + token
+    },
+    json: true
+};
+request.get(getOptions, function(error,response,body){
+    if(!error && response.statusCode === 200) {
+        console.log(body);
+    }
+    else{
+        console.log(response);
+    }
+});
+
+    

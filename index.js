cb3687db412f29473fa1eceea6ccc3595a8ef9db
Kai Vickers <kaiv14@comcast.net>
2022-03-28 08:02:27 -0700

    Modified styling of the graph

--- index.js
+++ index.js
@@ -1,40 +1,76 @@
-// import cytoscape from "cytoscape";
-
 let cy = cytoscape({
-  container: document.getElementById('cy'),
+  container: document.getElementById("cy"),
 
   elements: {
     nodes: [
       {
-        data: { id: 'a' }
+        data: { 
+          id: "a",
+          name: "James Blake"
+        },
       },
-
       {
-        data: { id: 'b' }
-      }
+        data: { 
+          id: "b",
+          name: "Bon Iver"
+        },
+      },
+      {
+        data: { 
+          id: "c",
+          name: "Bon Iver"
+        },
+      },
     ],
     edges: [
       {
-        data: { id: 'ab', source: 'a', target: 'b' }
-      }
-    ]
+        data: { id: "ab", source: "a", target: "b" },
+        data: { id: "ac", source: "a", target: "c" },
+        data: { id: "bc", source: "b", target: "c" },
+      },
+    ],
   },
+  zoomingEnabled: false,
   userZoomingEnabled: false,
+  panningEnabled: false,
   userPanningEnabled: false,
   boxSelectionEnabled: false,
-  autounselectify: true,
+  autoungrabify: false,
+  autounselectify: false,
+  touchTapThreshold: 8,
+  desktopTapThreshold: 4,
+  multiClickDebounceTime: 250,
   layout: {
-    name: 'grid',
-    rows: 1
+    name: "concentric",
+    //rows: 2,
   },
 
   // so we can see the ids
-  style: [
+  style: [ // the stylesheet for the graph
     {
       selector: 'node',
       style: {
-        'label': 'data(id)'
+        'background-color': '#666',
+        'label': 'data(name)'
       }
-    }
-  ]
+    },
+    {
+      selector: ':selected',
+      style: {
+        'width': 3,
+        'background-color': '#f00',
+        // 'target-arrow-color': '#ccc',
+        // 'target-arrow-shape': 'triangle',
+        // 'curve-style': 'bezier'
+      }
+    },
+  ],
+});
+
+cy.on("tap", "node", (e) => {
+  let node = e.cyTarget;
+  // node.style('background-color', '#f00')
+  let artisit = document.getElementById("artist");
+  artisit.innerHTML = node.data("name");
+
 });
